6bdae88a20be061590ca1ee441027956d9e1fca3
nattaphol-css <nattaphol@slri.or.th>
2017-04-28 13:26:31 +0700

    add header

--- SocialNetwork/components/SynHeader.js
+++ SocialNetwork/components/SynHeader.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import {
-  Header
+  Header,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Title,
+  Center,
+  Right,
+  Thumbnail
 } from 'native-base'
-
+import {
+  Image
+} from 'react-native'
 export default class SynHeader extends Component{
   render() {
     return (
       <Header>
-        
+      <Left>
+        <Button transparent>
+          <Icon name='menu' />
+        </Button>
+      </Left>
+      <Body style={{flex:1, alignItem: 'Center'}}>
+        <Thumbnail size={500} source={require('../public/slri.png')}/>
+      </Body>
       </Header>
     );
   }
